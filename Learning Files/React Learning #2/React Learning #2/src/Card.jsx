312aeea43142dade9b6506a9728f4f3c5310988c
Valentino Nathan <nathan070907@gmail.com>
2025-12-04 14:27:51 +0700

    React Learning #2 Created

---
+++ Learning Files/React Learning #2/React Learning #2/src/Card.jsx
@@ -0,0 +1,11 @@
+function Card() {
+    return (
+        <div className="card">
+            <img></img>
+            <h2>Valentino Nathan</h2>
+            <p>I am a CS student at NUS</p>
+        </div>
+    );
+}
+
+export default Card
