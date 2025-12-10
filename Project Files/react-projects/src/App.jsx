9eca5133704c7b6d465a6a45e60cce8213481313
Valentino Nathan <nathan070907@gmail.com>
2025-12-10 15:19:26 +0700

    React Projects

---
+++ Project Files/react-projects/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Route} from 'react-router-dom' 
+
+
+function App() {
+  return (
+    <p>Test</p>
+  );
+}
+
+export default App
